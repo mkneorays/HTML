30bc04ef5b3ff4a68f5e67d52ec6fc1b4f16c88d
Mithilesh Kumar <37469281+mkneorays@users.noreply.github.com>
2018-05-02 00:00:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -51,3 +51,46 @@ class FirstReact extends React.Component
 ReactDOM.render(<FirstReact/>,document.getElementById("root"))
 
 */
+
+/*
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+class Hello extends React.Component{
+	//u can define constructor also
+	constructor(){
+	  super(); //this is mandateroy
+	  this.name="davit";
+	  //this is a state
+	  this.state={
+	     firstname: 'Mithilesh Chandra'
+	  }
+	}
+render(){
+return (
+ <h1>Helo Good Morning  {this.name}
+   <p><World  detail={this.state.firstname}/></p>
+</h1>	
+
+)
+}//render method
+}//class
+
+class World extends React.Component
+{
+	getName(){
+	  return "anish";
+	}
+render(){
+   return(<p>MITHILESH <br/> {this.getName()}
+  <li>
+    {this.props.detail}	  
+  </li>
+   </p>   
+   )
+}//render
+}//class
+
+
+ReactDOM.render(<Hello/>,document.getElementById("root"))
+*/
