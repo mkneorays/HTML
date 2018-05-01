b9f2b17cf014f3b99870da74fa621e8489f3f589
Mithilesh Kumar <37469281+mkneorays@users.noreply.github.com>
2018-05-01 23:45:33 +0530

    Add files via upload
    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+
+/*
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+class Hello extends React.Component{
+	//u can define constructor also
+	constructor(){
+	  super(); //this is mandateroy
+	  this.name="davit";
+	  //this is a state
+	  this.state={
+	     firstname: 'Mithilesh Chandra'
+	  }
+	}
+render(){
+return (
+ <h1>Helo Good Morning  {this.name}
+   <p>{this.state.firstname}</p>
+</h1>	
+
+)
+}//render method
+}//class
+
+class World extends React.Component
+{
+	getName(){
+	  return "anish";
+	}
+render(){
+   return(<p>MITHILESH <br/> {this.getName()}
+
+   </p>   
+   )
+}//render
+}//class
+
+class FirstReact extends React.Component
+{
+	render(){
+	return(
+		<p>
+	    <Hello />
+		<World/>
+	    </p>
+	)
+	}//render
+};
+
+ReactDOM.render(<FirstReact/>,document.getElementById("root"))
+
+*/
