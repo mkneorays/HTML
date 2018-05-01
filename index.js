d9ee91d339543262171233749827e666bbcb3c29
Mithilesh Kumar <37469281+mkneorays@users.noreply.github.com>
2018-05-02 00:18:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -94,3 +94,59 @@ render(){
 
 ReactDOM.render(<Hello/>,document.getElementById("root"))
 */
+/*
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+class Hello extends React.Component{
+	//u can define constructor also
+	constructor(){
+	  super(); //this is mandateroy
+	  this.name="davit";
+	  //this is a state
+	  this.state={
+	     name1: ['Mithilesh',' Chandra','yougesh']
+	  }
+	}
+render(){
+return (
+ <h1>Helo Good Morning  {
+	this.state.name1.map(function(nam){
+		return<World key={nam}  detail={nam}/>
+	})//function
+		}//javascript syntax
+   
+</h1>	
+//key={nam} Warning: Each child in an array or iterator should have a unique "key" prop.
+)
+}//render method
+}//class
+
+class World extends React.Component
+{
+	
+render(){
+   return(
+  <li>
+    {this.props.detail}	  
+  </li>
+   
+   )
+}//render
+}//class
+
+
+ReactDOM.render(<Hello/>,document.getElementById("root"))
+
+//output
+/*Helo Good Morning
+Mithilesh
+Chandra
+yougesh
+javascript syntax
+*/
+*/
+
+
